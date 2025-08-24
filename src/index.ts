04a36a502a2b8a7257c38e9a58171984e4ac84bf
Hakim1brahim <abdihakimibrahim99@gmail.com>
2025-08-24 18:44:16 +0200

    fix:configure tsconfig and npm scripts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
-import express from "express";
+import express from "express"
 import {env} from "node:process"
 
-const app = express();
-const port: number = Number(env.PORT) || 3000;
+const app = express()
+const port: number = Number(env.PORT) || 3000
 const address: string = "0.0.0.0" // localhost - required for Render
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+const user: User = {
+  id: 1,
+  name: "Hakim",
+  email: "hakim@sti.com"
+}
+app.get("/user", (req, res) => {
+  res.status(201).json(user)
+})
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from Hakim" });
-});
-
-app.listen(port, () => {
-  console.log(`Listening to port ${port}`);
-});
+app.listen(port, address, () => {
+  console.log(`Listening to port ${port}`)
+})
